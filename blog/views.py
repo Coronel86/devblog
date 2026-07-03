9779c3a96dc34b1c4eff59ef43eb1ec175522dd9
HEBERTON HENRIQUE CORONEL DA SILVA <83427570053@senacrs.edu.br>
2026-07-02 21:43:27 -0300

    Criando Categorias

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,51 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Artigo
+from django.shortcuts import render, get_object_or_404
+from .models import Artigo, Categoria
 
 ## def home(request): ##
    ## return render(request, "blog/index.html")
 
 def home(request):
 
-    noticias = Artigo.objects.all()
+    categoria_selecionada = request.GET.get('categoria')
+
+    categorias = Categoria.objects.all()
     
+    if categoria_selecionada:
+        noticias = Artigo.objects.filter(categoria__nome__icontains=categoria_selecionada)
+    else:
+        noticias = Artigo.objects.all()
+
     contexto = {
-        'lista_artigos': noticias
+        'lista_artigos': noticias,
+        'lista_categorias': categorias,
+        'categoria_selecionada': categoria_selecionada
     }
     
     return render(request, "blog/index.html", contexto)
+   
+#def sobre_nos(request):
+    ##return render(request, "blog/sobre.html")
 
 def sobre_nos(request):
-    return render(request, "blog/sobre.html")
+    categorias = Categoria.objects.all()
+
+    contexto = {
+        'lista_categorias': categorias
+    }
+
+    return render(request, "blog/sobre.html", contexto)
+
+
+def artigo_detalhe(request, id):
+    categorias = Categoria.objects.all()
+
+    noticia = get_object_or_404(Artigo, id=id)
+
+    contexto = {
+        'lista_categorias': categorias,
+        'artigo': noticia
+    }
+
+    return render(request, 'blog/artigo_detalhe.html', contexto)
+
