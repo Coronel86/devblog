b26242cb31f4992d11267c26010eb00f3bef03a3
HEBERTON HENRIQUE CORONEL DA SILVA <83427570053@senacrs.edu.br>
2026-07-07 21:22:58 -0300

    aula 74

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,20 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('sobre/', views.sobre_nos, name='sobre_nos'),
     path('artigo/<int:id>/', views.artigo_detalhe, name='detalhe_artigo'),
+    path('contato/', views.fale_conosco, name='fale_conosco'),
+
+    path('admin/', admin.site.urls),
+
+    path('api/token/', TokenObtainPairView(), nam='token_obtain_pair'),
+
+    path('api/token/refresh/', TokenObtainPairView(), nam='token_obtain_pair'),
+
+    path('api/artigos/', views.api_listar_artigos, name='api_artigos'),    
+    path('api/artigo/novo/', views.api_criar_artigo),
 ]
--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
+}
